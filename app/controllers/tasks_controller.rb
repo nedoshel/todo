155ee183770228ca71d4db45c012fea46d25bf28
nedoshel <nedoshel@nedoshel-HP.(none)>
2014-04-08 18:14:22 +0400

    rsync

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,7 +29,7 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @task }
+        format.js { head :no_content }
       else
         format.html { render action: 'new' }
         format.json { render json: @task.errors, status: :unprocessable_entity }
@@ -43,7 +43,7 @@ class TasksController < ApplicationController
     respond_to do |format|
       if @task.update(task_params)
         format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { head :no_content }
+        format.js { head :no_content }
       else
         format.html { render action: 'edit' }
         format.json { render json: @task.errors, status: :unprocessable_entity }
@@ -55,9 +55,10 @@ class TasksController < ApplicationController
   # DELETE /tasks/1.json
   def destroy
     @task.destroy
+
     respond_to do |format|
       format.html { redirect_to tasks_url }
-      format.json { head :no_content }
+      format.js { head :no_content }
     end
   end
 
